3025a26895d55c4b1bcf367cad6902ff833ea296
skytech001 <abiodunlabinjo@gmail.com>
2023-01-06 17:50:26 -0600

    trouble shot

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import path from "node:path";
+import path from "path";
 import cors from "cors";
 import userRouter from "./routers/userRouter.js";
 import dotenv from "dotenv";
@@ -16,22 +16,20 @@ mongoose.connect("mongodb://localhost/centerspade", () => {
 });
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("server is ready");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
+// app.get("/", (req, res) => {
+//   res.send("server is ready");
+// });
 
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/orders", orderRouter);
 app.get("/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
-});
-
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
